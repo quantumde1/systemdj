da521d0dcdf5a0190d020acf7884bacf40909f4d
quantumde1 <meserakovarsenij810@gmail.com>
2023-06-27 15:52:46 +0300

    swdrfgthyyjuiklutcrxz

---
+++ source/daemonizator.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <syslog.h>
+
+void daemonizator() {
+    pid_t pid;
+
+    /* Fork off the parent process */
+    pid = fork();
+
+    /* An error occurred */
+    if (pid < 0) {
+        exit(EXIT_FAILURE);
+    }
+    /* Success: Let the parent terminate */
+    if (pid > 0) {
+        exit(EXIT_SUCCESS);
+    }
+    /* On success: The child process becomes session leader */
+    if (setsid() < 0) {
+        exit(EXIT_FAILURE);
+    }
+    /* Catch, ignore and handle signals */
+    //TODO: Implement a working signal handler */
+    signal(SIGCHLD, SIG_IGN);
+    signal(SIGHUP, SIG_IGN);
+
+    /* Fork off for the second time*/
+    pid = fork();
+
+    /* An error occurred */
+    if (pid < 0) {
+        exit(EXIT_FAILURE);
+    }
+    /* Success: Let the parent terminate */
+    if (pid > 0) {
+        exit(EXIT_SUCCESS);
+    }
+    /* Set new file permissions */
+    umask(0);
+
+    /* Change the working directory to the root directory */
+    /* or another appropriated directory */
+    chdir("/");
+
+    /* Close all open file descriptors */
+    for (int x = sysconf(_SC_OPEN_MAX); x>=0; x--) {
+        close (x);
+    }
+
+    /* Open the log file */
+    openlog("firstdaemon", LOG_PID, LOG_DAEMON);
+}
+
+int main() {
+    daemonizator();
+    while (true) {
+        system("systemdjctl autorun");
+    }
+    syslog (LOG_NOTICE, "Terminated");
+    closelog();
+    return 0;
+}
