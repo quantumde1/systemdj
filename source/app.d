b2466af47811f2d4094c70d3d40e44540d438048
quantumde1 <meserakovarsenij810@gmail.com>
2024-01-03 11:24:17 +0300

    Now, systemdj and systemdjctl is different repositories. Live with it.

--- source/app.d
+++ source/app.d
@@ -5,50 +5,26 @@ import std.utf;
 import std.process;
 import std.conv;
 import parse;
-import services.enable;
-import services.start;
-import services.disable;
-import services.logging;
 import services.autorun;
-import services.stop;
 import services.poweroff;
 import core.stdc.stdlib;
+import std.exception;
+import std.array;
+import core.sys.posix.sys.types;
+import core.sys.posix.unistd;
 
-void help() {
-	writeln("systemDJ initialization system.\n\nsystemdjctl [COMMAND] [ARGS]\n\ncommands: start stop enable disable journal clearjr autorun poweroff\n\nas arguments use service name");
-}
-
-void main(string[] args) {
-	switch (args[1]) {
-		case "start":
-			start(args[2]);
-			break;
-		case "enable":
-			enable(args[2]);
-			break;
-		case "help":
-			help();
-			break;
-		case "stop":
-			kill_process(args[2]);
-			break;
-		case "disable":
-			disable(args[2]);
-			break;
-		case "journal":
-			read_log();
-			break;
-		case "clearjr":
-			clear_log();
-			break;
-		case "autorun":
-			exec_all();
-			break;
-		case "poweroff":
-			stop_all();
-			break;
-		default:
-			writeln("No such command, try help");
-			break;
-	}
+void main()
+{
+    if (getpid() != 1) {
+        writeln("Run as 1 process, aka init");
+        exit(1);
+    }
+    writeln("Starting initialization!");
+    exec_all();
+    while (true)
+    {
+        auto p = spawnProcess("getty");
+        p.wait();
+        writeln("getty exited, restarting...");
+    }
 }
